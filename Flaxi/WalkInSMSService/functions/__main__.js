9d1794a3c5ac0332d529bd08cac5cdf792bb702b
GarlandZhang <garland._zhang@hotmail.com>
2018-01-27 19:43:25 -0500

    back and forth sms response

--- Flaxi/WalkInSMSService/functions/__main__.js
+++ Flaxi/WalkInSMSService/functions/__main__.js
@@ -1,18 +1,17 @@
 const lib = require('lib')({token:'m2RTVxgdm8r835hLA-l5VERydWYBPjF1FMzKuInbSwNYoA9b_LRO3ly9cQBR7C88'});
 /**
-* responding to a message from a patient
-* @param {string} smsService received message from patient. responding back to patient
-* @param {string} patient receiver of the automated response.
-* @param {string} message message
-* @returns {string}
+* @param {string} sender The phone number that sent the text to be handled
+* @param {string} receiver The StdLib phone number that received the SMS
+* @param {string} message The contents of the SMS
+* @param {string} createdDatetime Datetime when the SMS was sent
 */
-module.exports = (smsService = '12262123518', patient = '6472701402', message = 'received message', context, callback) => {
+module.exports = (sender = '6472701402', receiver = '12262123518', message = 'received message', createdDatetime = '', context, callback) => {
 
   //creat message that sends to smsService to patient
   lib.messagebird.tel.sms({
-    originator:  smsService,
-    recipient: patient,
-    body: 'message received. sending message back'
+    originator:  sender,
+    recipient: receiver,
+    body: message + " " + receiver + " " + sender
   }, (err, result) => {
     if (err) {
       return callback(err);
