ed76d30b6a08fec788fd7845479c86a629d4df24
GarlandZhang <garland._zhang@hotmail.com>
2018-01-28 04:24:05 -0500

    added database

--- Flaxi/WalkInSMSService/functions/__main__.js
+++ Flaxi/WalkInSMSService/functions/__main__.js
@@ -1,33 +1,100 @@
+//msg
 
-//
-let responseText = function (phoneNum, oldMsg, data){
+const lib = require('lib')({token:'m2RTVxgdm8r835hLA-l5VERydWYBPjF1FMzKuInbSwNYoA9b_LRO3ly9cQBR7C88'});
 
-  //msg
-  var jsonData = JSON.parse(data);
+function handleList(sender, receiver, message, callback) {
+  lib.utils.storage.get(sender, (err, aptData) => {
+    if(err){
+      return callback(err);
+    }
 
-  var msg = "";
+    lib['nim-wijetunga'].patient['@dev']({location: aptData.location, numResults: 2}, (err, results) => {
 
-  //1 -> list
+      var jsonData = JSON.parse(JSON.stringify(results, null, 2));
 
-  if(oldMsg.toLowerCase().includes("list")){
-    msg = "";
+      var msg = "";
 
 
-    for(var i = 0; i < jsonData.length; i++){
-      var clinic = jsonData[i];
+      for(var i = 0; i < jsonData.length; i++){
+        var clinic = jsonData[i];
 
-      var location = clinic.location;
+        var location = clinic.location;
+        var address = clinic.address_dest;
+        var dist = clinic.dist;
+        var name = clinic.clinic_name;
+        var waitTime = clinic.wait_time;
+          var hours = Math.floor(waitTime / 60);
+          var minutes = waitTime - 60 * hours;
+
+        msg = msg + (i + 1) + ". " + name + "\n";
+        msg = msg + "Address: " +  address.substring(0, address.substring(0, address.lastIndexOf(',')).lastIndexOf(','))+ "\n";
+        msg = msg + "Distance: " + dist[0] + ".\n";
+        msg = msg + "Travel Time: About " + dist[1] + ".\n";
+        msg = msg + "Wait Time: ";
+        if(hours == 0 && minutes == 0){
+          msg = msg + "is now!";
+        }
+        else if(hours == 0 && minutes > 1){
+          msg = msg + minutes + " minutes. \n";
+        }
+        else if(hours == 0 && minutes == 1 ){
+          msg = msg + "1 minute. \n";
+        }
+        else if(minutes == 0 && hours > 1){
+          msg = msg + hours + " hours. \n";
+        }
+        else if(minutes == 0 && hours == 1){
+          msg = msg + "1 hour. \n";
+        }
+        else if(hours == 1){
+          msg = msg + hours + " hour and " + minutes + " minutes. \n";
+        }
+        else if(minutes == 1){
+          msg = msg + hours + " hours and " + minutes + " minute. \n";
+        }
+        else {
+          msg = msg + hours + " hours and " + minutes + " minutes. \n";
+        }
+
+        msg = msg + "----------------------------------------------\n\n";
+
+      }
+
+      lib.messagebird.tel.sms({
+        originator:  receiver,
+        recipient: sender,
+        body: msg
+      }, (err, result) => {
+
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, result);
+      });
+    });
+  });
+};
+
+function handleBook(sender, receiver, message, callback) {
+  lib.utils.storage.get(sender, (err, aptData) => {
+    if (err) {
+      return callback(err);
+    }
+    lib['nim-wijetunga'].patient['@dev']({location: aptData.location, numResults: 2}, (err, results) => {
+      let clinic = results[parseInt(message.substring(message.length-1)) - 1];
+
       var address = clinic.address_dest;
       var dist = clinic.dist;
       var name = clinic.clinic_name;
       var waitTime = clinic.wait_time;
         var hours = Math.floor(waitTime / 60);
         var minutes = waitTime - 60 * hours;
+      var ranking = Math.random() * 40;
+      var totalPatients = Math.floor(Math.random() * 40 + ranking) + 1;
 
-      msg = msg + (i + 1) + ". " + name + "\n";
-      msg = msg + "Address: " +  address.substring(0, address.substring(0, address.lastIndexOf(',')).lastIndexOf(','))+ "\n";
-      msg = msg + "Distance: " + dist[0] + ".\n";
-      msg = msg + "Travel Time: About " + dist[1] + ".\n";
+      var msg = "";
+      msg += "Appt. at " + name + ".\n";
+      msg += "Location at: " + address.substring(0, address.substring(0, address.lastIndexOf(',')).lastIndexOf(',')) + ".\n";
       msg = msg + "Wait Time: ";
       if(hours == 0 && minutes == 0){
         msg = msg + "is now!";
@@ -53,70 +120,33 @@ let responseText = function (phoneNum, oldMsg, data){
       else {
         msg = msg + hours + " hours and " + minutes + " minutes. \n";
       }
+      msg += "Queued: " + totalPatients + " out of " + totalPatients + ".\n";
 
-      msg = msg + "----------------------------------------------\n\n";
+      lib.messagebird.tel.sms({
+        originator:  receiver,
+        recipient: sender,
+        body: msg
+      }, (err, result) => {
 
-    }
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, result);
+      });
+    });
+  })
+}
 
-  }
-  //2 -> book apt
-  else if(oldMsg.toLowerCase().includes("book apt")){
-    msg = "";
+function handleCancel(sender, receiver, message, callback){
+    var msg = "Your booking has been cancelled.";
+    return callback(null, msg);
+}
 
-    var c = parseInt(oldMsg.substring(oldMsg.length - 1) - 1);
-    if(c < 0 || c > jsonData.length)
-    {
-      return "ERRORRRR";
+function handleShow(sender, receiver, message, callback){
+  lib.utils.storage.get(sender, (err, aptData) => {
+    if(err){
+      return callback(err);
     }
-    var clinic = jsonData[c];
-
-    var address = clinic.address_dest;
-    var dist = clinic.dist;
-    var name = clinic.clinic_name;
-    var waitTime = clinic.wait_time;
-      var hours = Math.floor(waitTime / 60);
-      var minutes = waitTime - 60 * hours;
-    var ranking = Math.random() * 40;
-    var totalPatients = Math.floor(Math.random() * 40 + ranking) + 1;
-
-    msg += "Appt. at " + name + ".\n";
-    msg += "Location at: " + address.substring(0, address.substring(0, address.lastIndexOf(',')).lastIndexOf(',')) + ".\n";
-    msg = msg + "Wait Time: ";
-    if(hours == 0 && minutes == 0){
-      msg = msg + "is now!";
-    }
-    else if(hours == 0 && minutes > 1){
-      msg = msg + minutes + " minutes. \n";
-    }
-    else if(hours == 0 && minutes == 1 ){
-      msg = msg + "1 minute. \n";
-    }
-    else if(minutes == 0 && hours > 1){
-      msg = msg + hours + " hours. \n";
-    }
-    else if(minutes == 0 && hours == 1){
-      msg = msg + "1 hour. \n";
-    }
-    else if(hours == 1){
-      msg = msg + hours + " hour and " + minutes + " minutes. \n";
-    }
-    else if(minutes == 1){
-      msg = msg + hours + " hours and " + minutes + " minute. \n";
-    }
-    else {
-      msg = msg + hours + " hours and " + minutes + " minutes. \n";
-    }
-    msg += "Queued: " + totalPatients + " out of " + totalPatients + ".\n";
-
-  }
-  //3 -> cancel
-  else if(oldMsg.toLowerCase().includes("cancel")){
-
-    msg = "Your booking has been cancelled.";
-
-  }
-  //4 -> show apt
-  else if(oldMsg.toLowerCase().includes("show apt")){
 
     var ranking = Math.floor(Math.random() * 40) + 1;
     var totalPatients = Math.floor(Math.random() * 40) + ranking;
@@ -124,7 +154,7 @@ let responseText = function (phoneNum, oldMsg, data){
     var hours = Math.floor(waitTime / 60);
     var minutes = waitTime - 60 * hours;
 
-    msg = "Your current wait time is: ";
+    var msg = "Your current wait time is: ";
 
     if(hours == 0 && minutes == 0){
       msg = msg + "is now!";
@@ -152,41 +182,52 @@ let responseText = function (phoneNum, oldMsg, data){
     }
      msg = msg + "You are queued: " + ranking + "/" + totalPatients + ".\n";
 
-  }
-  //5 -> help
-  else if(oldMsg.toLowerCase().includes("help")){
-    msg = "Type 'list' to get list of nearby clinics and their wait times.\n\n"
-    + "Type 'book apt <insert appointment number>' to book an appointment.\n\n"
-    + "Type 'cancel' to cancel your appointment.\n\n"
-    + "Type 'show apt' to see your booked appointment.\n\n"
-  }
-  //error message
-  else{
+     lib.messagebird.tel.sms({
+       originator:  receiver,
+       recipient: sender,
+       body: msg
+     }, (err, result) => {
 
-    msg = "Error: not a valid command. Type 'help' for a list of commands.";
-
-  }
-
-  return msg;
-
+       if (err) {
+         return callback(err);
+       }
+       return callback(null, result);
+     });
+   });
 }
 
-const lib = require('lib')({token:'m2RTVxgdm8r835hLA-l5VERydWYBPjF1FMzKuInbSwNYoA9b_LRO3ly9cQBR7C88'});
+function handleHelp(sender, receiver, message, callback){
+  var msg = "Type 'list' to get list of nearby clinics and their wait times.\n\n"
+  + "Type 'book apt <insert appointment number>' to book an appointment.\n\n"
+  + "Type 'cancel' to cancel your appointment.\n\n"
+  + "Type 'show apt' to see your booked appointment.\n\n";
 
-/**
-* @param {string} sender The phone number that sent the text to be handled
-* @param {string} receiver The StdLib phone number that received the SMS
-* @param {string} message The contents of the SMS
-* @param {string} createdDatetime Datetime when the SMS was sent
-*/
-module.exports = (sender = '6472701402', receiver = '12262123518', message = 'help', createdDatetime = '', context, callback) => {
+  lib.messagebird.tel.sms({
+    originator:  receiver,
+    recipient: sender,
+    body: msg
+  }, (err, result) => {
+    if (err) {
+      return callback(err);
+    }
 
-  lib['nim-wijetunga'].patient['@dev']({location: 'Toronto', numResults: 2}, (err, results) => {
+    return callback(null, result);
+  });
+}
+
+function handleLocation(sender, receiver, location, callback){
+
+  var jsonObjectToStore = { location: location };
+
+  lib.utils.storage.set(sender, jsonObjectToStore, (err, result) => {
+    if (err) {
+      return callback(err);
+    }
 
     lib.messagebird.tel.sms({
       originator:  receiver,
       recipient: sender,
-      body: responseText(sender, message, JSON.stringify(results, null, 2))
+      body: location + " is saved as your location."
     }, (err, result) => {
 
       if (err) {
@@ -194,7 +235,33 @@ module.exports = (sender = '6472701402', receiver = '12262123518', message = 'he
       }
       return callback(null, result);
     });
-
   });
 
+}
+/**
+* @param {string} sender The phone number that sent the text to be handled
+* @param {string} receiver The StdLib phone number that received the SMS
+* @param {string} message The contents of the SMS
+* @param {string} createdDatetime Datetime when the SMS was sent
+*/
+module.exports = (sender = '6472701402', receiver = '12262123518', message = 'book apt 2', createdDatetime = '', context, callback) => {
+
+    if(message.toLowerCase().includes("list")) {
+        return handleList(sender, receiver, message, callback);
+      }
+    else if(message.toLowerCase().includes("book apt")){
+        return handleBook(sender, receiver, message, callback);
+    }
+   else if(message.toLowerCase().includes("cancel")){
+        return handleCancel(sender, receiver, message, callback);
+   }
+   else if(message.toLowerCase().includes("show apt")){
+        return handleShow(sender, receiver, message, callback);
+   }
+   else if(message.toLowerCase().includes("help")){
+        return handleHelp(sender, receiver, message, callback);
+   }
+   else{
+        return handleLocation(sender, receiver, message, callback);
+   }
 };
