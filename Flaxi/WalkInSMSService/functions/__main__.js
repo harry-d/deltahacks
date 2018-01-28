09129981fa892e0d20c26134fe8339de065b1e6a
GarlandZhang <garland._zhang@hotmail.com>
2018-01-27 20:12:20 -0500

    able to customize messages

--- Flaxi/WalkInSMSService/functions/__main__.js
+++ Flaxi/WalkInSMSService/functions/__main__.js
@@ -1,23 +1,49 @@
-function text(phoneNum, msg){
+//
+let responseText = function (phoneNum, msg){
 
+  //1 -> book msg
+  if(msg == "1"){
 
+     return msg;
+  }
+  //2 -> cancel message
+  else if(msg == "2"){
+
+    return msg + " asdsakdjhaslkaslkhdj";
+  }
+  //3 -> check wait time
+  else if(msg == "3"){
+
+  }
+  //4 -> help
+  else if(msg == "4"){
+
+  }
+  //error message
+  else {
+
+  }
+
+  return 'nothing';
 
 }
 
 const lib = require('lib')({token:'m2RTVxgdm8r835hLA-l5VERydWYBPjF1FMzKuInbSwNYoA9b_LRO3ly9cQBR7C88'});
+
 /**
 * @param {string} sender The phone number that sent the text to be handled
 * @param {string} receiver The StdLib phone number that received the SMS
 * @param {string} message The contents of the SMS
 * @param {string} createdDatetime Datetime when the SMS was sent
 */
-module.exports = (sender = '6472701402', receiver = '12262123518', message = 'received message', createdDatetime = '', context, callback) => {
+module.exports = (sender = '6472701402', receiver = '12262123518', message = '2', createdDatetime = '', context, callback) => {
 
   lib.messagebird.tel.sms({
     originator:  receiver,
     recipient: sender,
-    body: message + " " + receiver + " " + sender
+    body: responseText(sender, message)
   }, (err, result) => {
+    console.log(result);
     if (err) {
       return callback(err);
     }
