1eb8e86f4dbab7c7c4d00af6d00956c5a4f8d836
GarlandZhang <garland._zhang@hotmail.com>
2018-01-27 23:04:54 -0500

    todo work for formatting texts

--- Flaxi/WalkInSMSService/functions/__main__.js
+++ Flaxi/WalkInSMSService/functions/__main__.js
@@ -1,33 +1,73 @@
 //
-let responseText = function (phoneNum, msg){
+let responseText = function (phoneNum, msg, data){
 
   //msg
-  var data = JSON.parse();
+  var jsonData = JSON.parse(data);
 
-  //1 -> book msg
+  var newMsg = "";
+
+  //1 -> list
   if(msg == "1"){
 
+    for(var i = 0; i < jsonData.length; i++){
+      var clinic = jsonData[i];
+
+      var location = clinic.location;
+      var dest = clinic.dest;
+      var dist = clinic.dist;
+      var name = clinic['clinic name'];
+
+      msg = msg + i + ". " + name + "\n";
+      msg = msg + "Location: " + location + "\n";
+      msg = msg + "Destination: " + dest + "\n";
+      msg = msg + "Distance: " + dist + "\n";
+      msg = msg + "---------------------------\n\n";
+    }
 
   }
-  //2 -> cancel message
+  //2 -> book apt
   else if(msg == "2"){
 
-    return msg + " asdsakdjhaslkaslkhdj";
+    /*
+    book apt 2
+
+    jsonData[2]
+
+
+
+    */
   }
-  //3 -> check wait time
+  //3 -> cancel
   else if(msg == "3"){
 
+    msg = "Cancelled booking";
+
   }
-  //4 -> help
+  //4 -> check wait time
   else if(msg == "4"){
+
+    var ranking = Math.random() * 40;
+    var totalPatients = Math.random() * 40 + ranking;
+    var waitTime = Math.random() * 40;
+
+    msg = "Total wait time is: " + waitTime + " You are queued: " + ranking + "/" + totalPatients;
+
+  }
+  //5 -> help
+  else if(msg == "5"){
+
+    msg = "Type 'list' to get list of appointments." + "\n"
+    + "Type 'book apt <insert appointment number>' to book an appointment." + "\n"
+    + "Type 'cancel' to cancel appointment." + "\n"
+    + "Type 'wait time' to get wait time and queue status" + "\n";
 
   }
   //error message
   else {
-
+    msg = "ERRORRRRRRRRRRRRRRRRRRRRR";
   }
 
-  return 'nothing';
+  return msg;
 
 }
 
@@ -39,16 +79,16 @@ const lib = require('lib')({token:'m2RTVxgdm8r835hLA-l5VERydWYBPjF1FMzKuInbSwNYo
 * @param {string} message The contents of the SMS
 * @param {string} createdDatetime Datetime when the SMS was sent
 */
-module.exports = (sender = '6472701402', receiver = '12262123518', message = '2', createdDatetime = '', context, callback) => {
+module.exports = (sender = '6472701402', receiver = '12262123518', message = '1', createdDatetime = '', context, callback) => {
 
-  lib['nim-wijetunga'].patient['@dev']({location: 'San Francisco'}, (err, results) => {
+  lib['nim-wijetunga'].patient['@dev']({location: 'Toronto', numResults: 2}, (err, results) => {
 
     lib.messagebird.tel.sms({
       originator:  receiver,
       recipient: sender,
-      body: JSON.stringify(results, null, 2)
+      body: responseText(sender, message, JSON.stringify(results, null, 2))
     }, (err, result) => {
-      console.log(result);
+
       if (err) {
         return callback(err);
       }
