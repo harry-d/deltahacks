316819b6b74fe59a0efcc91c73648dd732ba29b9
vnwijetu <vnwijetu@uwaterloo.ca>
2018-01-28 10:22:52 -0500

    Finished Web App

--- js/deltahacks.js
+++ js/deltahacks.js
@@ -10,13 +10,13 @@ $( document ).ready(function() {
         +'http://www.momgoesgreen.com/wp-content//hospital-building.jpg'+
         '" class="img-responsive img-rounded"/></div><div class="col-sm-8"><h2>'
         +data.clinic_name+
-        '</h2><p>'
-        +data.dist[0]+ ", "+data.dist[1]+" away"
+        '</h2><p style="margin:5px;">'
+        +data.dist[0]+ ", "+data.dist[1]+" away"+
         '</p><p>'
         +data.wait_time+
-        '</p></div></div>';
+        ' minute wait time</p></div></div>';
 
-        $("#card-wrapper").html(card);
+        $("#card-wrapper").append(card);
     }
 
 
@@ -27,9 +27,9 @@ $( document ).ready(function() {
 
         if(location == "") location = "Toronto";
 
-        lib['nim-wijetunga'].patient['@dev']({location:location, numResults:2}, (err, result) => {
+        lib['nim-wijetunga'].patient['@dev']({location:location, numResults:3}, (err, result) => {
             if(!err){
-                var data = JSON.parse(JSON.stringify(result, null, 2));
+                data = JSON.parse(JSON.stringify(result, null, 2));
                 var index = 1;
                 for(var i in data){
                     console.log(data[i]);
@@ -47,24 +47,60 @@ $( document ).ready(function() {
 
     $("body").css("background", "#eee").delay(2000);
 
-    $("#card1").click(function(){
-        $("#info").css("transform", "translateY(0%)");
-    });
 
     $("#close").click(function(){
         $("#info").css("transform", "translateY(100%)");
     });
 
-    $(".card").click(function(){
-        var id = $(this).attr('id');
-        id = parseInt(id.substring(4));
-        $("#info-h2").text(data[id]['location']);
-        $("#info-distance").text(data[id]['distance'][0]+ ", "+data[id]['distance'][1]+" away");
-        $("#info-wait").text(data[id]['wait_time']);
-        $("#info-btn").text("Book earliest available time ("+data[id]['wait_time']+" from now)");
-        $("#info-btn-custom").text(data[id]['location']);
-    })
+    $(function(){
+        $('body').on({ click : function(){
+                var id = 0;
+                $("#info-h2").text(data[id].clinic_name);
+                $("#info-distance").text(data[id].dist[0]+ ", "+data[id].dist[1]+" away");
+                $("#info-wait").text(data[id].wait_time +" minutes from now");
+                $("#book-btn").text("Book earliest available time ("+data[id].wait_time+" minutes from now)");
+                $("#info").css("transform", "translateY(0%)");
+                lastBtn = id;
+            }   
+        },'#card1');
+        console.log("ues");
+    });
 
+    $(function(){
+        $('body').on({ click : function(){
+                var id = 1;
+                $("#info-h2").text(data[id].clinic_name);
+                $("#info-distance").text(data[id].dist[0]+ ", "+data[id].dist[1]+" away");
+                $("#info-wait").text(data[id].wait_time +" minutes from now");
+                $("#book-btn").text("Book earliest available time ("+data[id].wait_time+" minutes from now)");
+                $("#info").css("transform", "translateY(0%)");
+                lastBtn = id;
+            }   
+        },'#card2');
+        console.log("ues");
+    });
+
+    $(function(){
+        $('body').on({ click : function(){
+                var id = 2;
+                $("#info-h2").text(data[id].clinic_name);
+                $("#info-distance").text(data[id].dist[0]+ ", "+data[id].dist[1]+" away");
+                $("#info-wait").text(data[id].wait_time +" minutes from now");
+                $("#book-btn").text("Book earliest available time ("+data[id].wait_time+" minutes from now)");
+                $("#info").css("transform", "translateY(0%)");
+                lastBtn = id;
+            }   
+        },'#card3');
+        console.log("ues");
+    });
+
+
+ 
+    $("#book-btn").click(function(){
+
+        $("#book-btn").text("Booked!").delay(1800);
+        data[lastBtn].wait_time += 5;
+    });
 
 
 
